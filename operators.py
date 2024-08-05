d311f444f80bf1aa734b281ae06dd4e72a26aeed
Kakesokk <Kakesozigb@gmail.com>
2024-08-05 15:36:33 -0500

    intial commit

---
+++ operators.py
@@ -0,0 +1,8 @@
+print(2+2)
+print(7-2)
+print(78*54)
+print(56/8)
+print(56//8)
+print(876565//3)
+print(9**3)
+print(5%4)
---
+++ variables.py
@@ -0,0 +1,8 @@
+number = 678
+print(number)
+decimal = 6.23
+print(decimal)
+quotes = "books:)XD123456782345678345678"
+print(quotes)
+trick = True
+print(trick)
